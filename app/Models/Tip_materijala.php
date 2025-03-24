6baf73dfb9022c508c59cc71a2f7dc5d9714a59d
DESKTOP-DFKEU89\filip <filipovicbojana2@gmail.com>
2025-03-24 21:01:30 +0100

    Model tip_materijala

---
+++ app/Models/Tip_materijala.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tip_materijala extends Model
+{
+    /**
+     * Tabela koja predstavalja model
+     * 
+     * @var string
+     */
+    protected $table = 'tip_materijala';
+
+    /**
+     * Primarni kljuc u tabeli
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'tip_materijala_id';
+
+
+    /**
+     * Iskljucivanje created_at i updated_at kolone
+     * 
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Kolone u koje je dozvoljen upis
+     */
+    protected $fillable = ['naziv'];
+
+    /**
+     * Vraca sve departmane iz tabele Departmani
+     */
+    public static function getTipMaterijala()
+    {
+        return self::all();
+    }
+}
