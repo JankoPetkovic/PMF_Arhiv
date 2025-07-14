dd2ec0d4f543d815abe3cfa6862a2397ed8be03f
JANKO-PC\Janko <jankopetkovic13@gmail.com>
2025-07-14 20:01:52 +0200

    responzivnost home stranice

--- resources/js/Komponente/Departmani.jsx
+++ resources/js/Komponente/Departmani.jsx
@@ -23,50 +23,55 @@ export default function Departmani({smerovi})
 
     
   return (
-    <>
-      <div className="flex border-2 rounded-xl h-20 gap-7 items-center justify-center shadow-[12px_12px_14px_-1px_rgba(0,_0,_0,_0.1)]">
-        {smerovi.map(departman => (
-          <div key={departman.departman_id} className="relative">
-            <button 
-              className="p-5 hover:bg-blue-100 relative rounded-md cursor-pointer hover:scale-110 transition-transform duration-200"  
-              onClick={() => toggleDropdown(departman.departman_id)}
-              onMouseEnter={() => setActiveDropdown(departman.departman_id)}
-            >
-              <img src={`/storage/ikonice/${departman.departman_naziv.toLowerCase().replace(/ /g, "_")}` + ".svg"} className="inline-block w-10 h-10 mr-2 align-middle"/>
-              {departman.departman_naziv}
-            </button>
-            
-            {activeDropdown === departman.departman_id && (
-              <div className="absolute left-0 mt-1 w-48 bg-white border border-gray-200 rounded-md shadow-[12px_12px_14px_-1px_rgba(0,_0,_0,_0.1)] z-10 ">
-                <ul className="" onMouseLeave={() => setActiveDropdown(null)}>
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:flex gap-4 border-2 rounded-xl p-4 shadow-[12px_12px_14px_-1px_rgba(0,_0,_0,_0.1)]">
+      {smerovi.map(departman => (
+        <div key={departman.departman_id} className="relative">
+          <button 
+            className="w-full flex items-center gap-3 px-4 py-3 hover:bg-blue-100 rounded-md cursor-pointer hover:scale-105 transition-transform duration-200"
+            onClick={() => toggleDropdown(departman.departman_id)}
+            onMouseEnter={() => setActiveDropdown(departman.departman_id)}
+          >
+            <img
+              src={`/storage/ikonice/${departman.departman_naziv.toLowerCase().replace(/ /g, "_")}.svg`}
+              className="w-8 h-8"
+              alt={departman.departman_naziv}
+            />
+            <span className="whitespace-nowrap">{departman.departman_naziv}</span>
+          </button>
+
+          {activeDropdown === departman.departman_id && (
+            <div className="absolute left-0 mt-1 w-48 bg-white border border-gray-200 rounded-md shadow-[12px_12px_14px_-1px_rgba(0,_0,_0,_0.1)] z-10">
+              <ul onMouseLeave={() => setActiveDropdown(null)}>
                 {Object.entries(departman.nivo_studija).map(([nivo, smerovi]) => (
-                <li key={nivo}>
-                    <button className="p-2 hover:bg-blue-100 cursor-pointer rounded-lg"
-                    onClick={() => toggleNivo(nivo)}>
-                    {nivo}
+                  <li key={nivo}>
+                    <button
+                      className="p-2 hover:bg-blue-100 cursor-pointer rounded-lg w-full text-left"
+                      onClick={() => toggleNivo(nivo)}
+                    >
+                      {nivo}
                     </button>
                     {openNivoi[nivo] && (
-                    <ul className="ml-4 mt-2 list-disc text-gray-700 p-2 ">
+                      <ul className="ml-4 mt-2 list-disc text-gray-700 p-2">
                         {smerovi.map((smer) => (
-                        <li key={smer.id} className="hover:bg-blue-100 rounded-lg p-1">
+                          <li key={smer.id} className="hover:bg-blue-100 rounded-lg p-1">
                             <Link
-                                href={`/smerovi/${smer.id}`}
-                                className="text-blue-500"
-                                >{smer.naziv}
+                              href={`/smerovi/${smer.id}`}
+                              className="text-blue-500"
+                            >
+                              {smer.naziv}
                             </Link>
-                        </li>
+                          </li>
                         ))}
-                    </ul>
+                      </ul>
                     )}
-                </li>
+                  </li>
                 ))}
-                </ul>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </>
+              </ul>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
   );
 };
 
--- resources/js/Komponente/PrikazMaterijala.jsx
+++ resources/js/Komponente/PrikazMaterijala.jsx
@@ -5,13 +5,13 @@ import KarticaMaterijala from './KarticaMaterijala';
 
 export default function PrikazMaterijala({predmeti = false, materijali}){
     return(
-        <div className="flex flex-wrap justify-start items-start gap-4 p-4 h-[80vh] overflow-auto">
+        <div className="flex flex-wrap gap-4 p-4 h-[80vh] overflow-auto justify-center lg:justify-start">
             {Array.isArray(materijali) && materijali.length !== 0 && (
                 materijali.map((m) => (
-                    <KarticaMaterijala
-                        materijal={m}
-                        key={m.materijal_id + 0.1}
-                    />
+                <KarticaMaterijala
+                    materijal={m}
+                    key={m.materijal_id}
+                />
                 ))
             )}
         </div>
